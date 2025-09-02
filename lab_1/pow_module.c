c8aab5acacdea0fd89c3a28d8a5fc8dfb52d6d0b
flammemi <sahadell@mail.ru>
2025-09-02 18:22:10 +0700

    fix pow

--- lab_1/pow_module.c
+++ lab_1/pow_module.c
@@ -4,18 +4,19 @@
 long long pow_by_module(long long a, long long exp, long long module) {
   long long res = 1;
   long long dp = a;
-  for (int i = 0; i <= log(exp); i++) {
+  // printf("%lf\n", log2(exp));
+  for (int i = 0; i <= log2(exp); i++) {
     if ((exp >> i) & 1) {
-      res = res * dp % module;
+      res = (res * dp) % module;
     }
-    dp = dp * dp % module;
+    dp = (dp * dp) % module;
   }
-  return res;
+  return res; 
 }
 
 int main() {
-  long long digit = 2;
-  long long exp = 100;
-  long long module = 13;
+  long long digit = 234234;
+  long long exp = 324;
+  long long module = 3555;
   printf("%lld\n", pow_by_module(digit, exp, module));
 }
